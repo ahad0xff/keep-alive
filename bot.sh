005026d31b4992c5814e2fbff1784dc35b0eb5d0
Λ ꫝ Λ Đ <93983627+ahadz-git@users.noreply.github.com>
2022-01-23 15:04:38 +0530

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/ahadz-git/mirror-leech-telegram-bot.git /app; cd /app
-curl -sL https://raw.githubusercontent.com/ahadz-git/nothing/main/mirrortest/config.env -o config.env
+git clone https://github.com/ahadz-git/mltb.git /app; cd /app
 docker build . --rm --force-rm --compress --no-cache=true --pull --file Dockerfile -t bot
 docker run --privileged --rm -i bot
