e811f94a2f0acbb4fe6a4a6fa200e93e216a92f0
Λ ꫝ Λ Đ <93983627+ahadz-git@users.noreply.github.com>
2022-01-28 22:23:47 +0530

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,4 +1,3 @@
-###
 git clone https://github.com/ahadz-git/TorToolkitX.git /app; cd /app
 docker build . --rm --force-rm --compress --no-cache=true --pull --file Dockerfile -t bot
 docker run --privileged --rm -i bot
